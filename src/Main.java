0e926eac257a4569c667997347b60ce579201017
Locku <lockup86@rambler.ru>
2024-05-18 15:22:48 +0500

    HM10.2

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        {
-            // Задание №1
 
-            String firstName = "Ivan";
-            String middleName = "Ivanovich";
-            String lastName = "Ivanov";
-            String fullName = lastName + " " + firstName + " " + middleName;
-            System.out.println("Ф. И. О. сотрудника — " + fullName);
+        // Задание №1
 
-            System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
-            // Задание №2
-            String upperFullName = fullName.toUpperCase();
-            System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + upperFullName);
-            // ну или  чтоб не создавать новую переменную System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println();
 
-            System.out.println();
-        }
+        // Задание №2
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + upperFullName);
+        // ну или  чтоб не создавать новую переменную System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println();
+
 
         // Задание №3
-        String fullName = "Иванов Семён Семёнович";
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace('ё' , 'е'));
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName1.replace('ё' , 'е'));
 
 
 
